c12b8aac05eead1fda1075f8f4b55345cb66eb04
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-07 20:43:36 +0300

    Implement adding tasks functionality

--- src/modules/taskDisplay.js
+++ src/modules/taskDisplay.js
@@ -1,16 +1,25 @@
-import tasks from './taskObjects.js';
 import menuIcon from '../img/menu-icon.svg';
+import deleteIcon from '../img/icons8-trash.svg';
+import Task from './Task.js';
 
 const inputs = document.querySelector('.task-input-item');
 
 const tasksDisplay = () => {
-  tasks.sort((a, b) => a.index - b.index).reverse().forEach((task) => {
+
+  if (JSON.parse(localStorage.getItem('taskList'))) {
+    Task.tasks = JSON.parse(localStorage.getItem('taskList'));
+  }
+
+  Task.tasks.sort((a, b) => a.index - b.index).reverse().forEach((task) => {
     const html = `
       <li class="task-item">
         <div class="task">
           <input type="checkbox" name="task"> ${task.description}
         </div>
-        <img src="${menuIcon}" alt="option-icon" class="menu-icon">
+        <div>
+          <img src="${menuIcon}" alt="option-icon" class="menu-icon">
+          <img src="${deleteIcon}" alt="delete-icon" class="delete-icon">
+        </div>
       </li>
       `;
     inputs.insertAdjacentHTML('afterend', html);
